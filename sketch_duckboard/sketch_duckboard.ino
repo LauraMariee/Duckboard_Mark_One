357fd77e1e9c1988a02ead88366d92f84f151cae
Luca Vazzano <luca.vazzano@gmx.net>
2021-01-30 21:55:52 +0100

    add first testing sketch

---
+++ sketch_duckboard/sketch_duckboard.ino
@@ -0,0 +1,23 @@
+const int buttonPin = 8;
+const int outPin =  7;
+
+int buttonState = 0;
+
+void setup() {
+  pinMode(outPin, OUTPUT);
+  digitalWrite(outPin, HIGH);
+  
+  pinMode(buttonPin, INPUT);
+
+  Serial.begin(9600);
+  Serial.println("\n\noh hi\n\n");
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    Serial.println("YEET");
+  } else {
+    Serial.println("----");
+  }
+}
